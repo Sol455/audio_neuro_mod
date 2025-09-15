0d80fcf947dbb324b8478915e8c4d846e33d94bd
Sol455 <sol@glowinthedark.co.uk>
2025-09-15 15:35:59 +0100

    Audio Engine class sketch

---
+++ Source/dsp/AudioEngine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sol Harter on 15/09/2025.
+//
+
+#include "AudioEngine.h"
---
+++ Source/dsp/AudioEngine.h
@@ -0,0 +1,13 @@
+//
+// Created by Sol Harter on 15/09/2025.
+//
+
+#ifndef AUDIO_NEURO_MOD_AUDIOENGINE_H
+#define AUDIO_NEURO_MOD_AUDIOENGINE_H
+
+
+class AudioEngine {
+};
+
+
+#endif //AUDIO_NEURO_MOD_AUDIOENGINE_H
